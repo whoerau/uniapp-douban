4d85aaad18b8fa33be9a09f8b1229e26e8146115
yangwl <yangwl@zonekey.com.cn>
2020-03-12 18:27:53 +0800

    录音

--- pages/book-video/book-video.vue
+++ pages/book-video/book-video.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		
+		<canvas style="width: 750upx; height: 200px;" canvas-id="canvasIdFlag" disable-scroll="false" @touchstart="handleTouchStart"
+		 @touchmove="handleTouchMove" @touchend="handleTouchEnd" @touchcancel="handleTouchCancel" @longtap="handleLongTap"
+		 @error="handleErr"></canvas>
 	</view>
 </template>
 
@@ -8,12 +10,57 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
+		},
+		onReady() {
+			console.log(`onready触发`)
+			this.draw()
+		},
+		methods: {
+
+			handleTouchStart() {
+				console.log(`手指触摸动作开始`)
+			},
+			handleTouchMove() {
+				console.log(`手指触摸动作移动`)
+			},
+			handleTouchEnd() {
+				console.log(`手指触摸动作结束`)
+			},
+			handleTouchCancel() {
+				console.log(`手指触摸动作被打断`)
+			},
+			handleLongTap() {
+				console.log(`长按`)
+			},
+			handleErr() {
+				console.log(`发生错误的时候`)
+			},
+			draw() {
+				let context = uni.createCanvasContext('canvasIdFlag')
+				context.setStrokeStyle("#00ff00")
+				context.setLineWidth(5)
+				context.rect(0, 0, 200, 200)
+				context.stroke()
+				context.setStrokeStyle("#ff0000")
+				context.setLineWidth(2)
+				context.moveTo(160, 100)
+				context.arc(100, 100, 60, 0, 2 * Math.PI, true)
+				context.moveTo(140, 100)
+				context.arc(100, 100, 40, 0, Math.PI, false)
+				context.moveTo(85, 80)
+				context.arc(80, 80, 5, 0, 2 * Math.PI, true)
+				context.moveTo(125, 80)
+				context.arc(120, 80, 5, 0, 2 * Math.PI, true)
+				context.stroke()
+				context.draw()
+			}
+
 		}
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 </style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,63 @@
 <template>
 	<view>
-		
+		<!-- 基础用法 -->
+		<uni-swipe-action>
+			<uni-swipe-action-item :options="options" @click="onClick" @change="change">
+				<view class='cont'>每一item项212122</view>
+			</uni-swipe-action-item>
+			<uni-swipe-action-item :options="options" @click="onClick" @change="change">
+				<view class='cont'>每一item项212122</view>
+			</uni-swipe-action-item>
+			<uni-swipe-action-item :options="options" @click="onClick" @change="change">
+				<view class='cont'>每一item项212122</view>
+			</uni-swipe-action-item>
+		</uni-swipe-action>
+
+
+
 	</view>
 </template>
 
 <script>
+	import uniSwipeAction from '@/components/uni-swipe-action/uni-swipe-action.vue'
+	import uniSwipeActionItem from '@/components/uni-swipe-action-item/uni-swipe-action-item.vue'
 	export default {
-		data() {
-			return {
-				
-			};
+		components: {
+			uniSwipeAction,
+			uniSwipeActionItem
+		},
+		data: () => ({
+			options: [ {
+				text: '删除	',
+				style: {
+					backgroundColor: '#FF4645'
+				}
+			}]
+		}),
+		methods: {
+			onClick(e) {
+				console.log('当前点击的是第' + e.index + '个按钮，点击内容是' + e.content.text)
+				uni.showModal({
+				    title: '提示',
+				    content: '此操作不可逆，是否确认删除',
+				    success: (res)=> {
+				        if (res.confirm) {
+				            console.log('用户点击确定');
+				        } else if (res.cancel) {
+				            console.log('用户点击取消');
+				        }
+				    }
+				});
+			},
+			change(open) {
+				// console.log('当前开启状态：' + open)
+			}
 		}
 	}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.cont{
+	// background-color: pink;
+}
 </style>
